657e7b6d18afc63e60516d953bd4d358a9d7dcfc
Hilppah <101551721+Hilppah@users.noreply.github.com>
2022-11-01 00:41:10 +0200

    Add files via upload

---
+++ viikko 6/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer(){
+    return (
+    <footer classname='Footer'>
+        <div className='footer'>
+            <span>Place sticky footer here</span>
+        </div>
+    </footer>
+    );
+}
